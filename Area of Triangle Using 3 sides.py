2e0d4ca64191f773ab2ca3bbe7081aa9702238bf
Prashant Bansal <49639153+bansal03@users.noreply.github.com>
2019-04-16 09:53:29 +0530

    Create Area of Triangle Using 3 sides.py

---
+++ Area of Triangle Using 3 sides.py
@@ -0,0 +1,10 @@
+# area of triangle using 3 sides
+
+
+import math
+a=int(input("Enter first side: "))
+b=int(input("Enter second side: "))
+c=int(input("Enter third side: "))
+s=(a+b+c)/2
+area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+print("Area of the triangle is: ",round(area,2))
